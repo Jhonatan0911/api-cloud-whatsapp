d1118ae82bb807f83b3cd53b9b3d090b196cad3c
Jhonatan Piña <jpinalozano911@gmail.com>
2022-07-18 10:22:27 -0500

    make it better

--- src/saveDatabase/sendUser/request.module.ts
+++ src/saveDatabase/sendUser/request.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RequestService } from './request.service';
 import { RequestController } from './request.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { RequestSchema } from 'src/saveDatabase/sendUser/schemas/Request.schema';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: 'Request', schema: RequestSchema }]),
     ],
     controllers: [ 
         RequestController 
