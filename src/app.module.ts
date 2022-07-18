e83bee3a857529475a9a4598ee127162f97ac491
Jhonatan Piña <jpinalozano911@gmail.com>
2022-07-18 10:28:25 -0500

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,11 @@ import { AppService } from './app.service';
 import { ShareModuleModule } from './share_module/share_module.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
-import { RequestSchema } from './saveDatabase/sendUser/schemas/request.schema';
 import { RequestModule } from './saveDatabase/sendUser/request.module';
 @Module({
   imports: [ 
     ShareModuleModule,
     RequestModule,
-    MongooseModule.forFeature([{ name: 'Request', schema: RequestSchema }]),
     MongooseModule.forRoot('mongodb+srv://root:1234@cluster0.zcerhsd.mongodb.net/?retryWrites=true&w=majority'),
   ],
   controllers: [ 
--- src/saveDatabase/sendUser/request.module.ts
+++ src/saveDatabase/sendUser/request.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RequestService } from './request.service';
 import { RequestController } from './request.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RequestSchema } from 'src/saveDatabase/sendUser/schemas/Request.schema';
 
 @Module({
     imports: [
+        MongooseModule.forFeature([{ name: 'Request', schema: RequestSchema }]),
     ],
     controllers: [ 
         RequestController 
