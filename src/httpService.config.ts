f9a7a371dfbce2b9cf8e439a6e7849b1a3a3466a
Jhonatan Piña <jpinalozano911@gmail.com>
2022-07-22 17:53:36 -0500

    cambios

--- src/httpService.config.ts
+++ src/httpService.config.ts
@@ -6,7 +6,7 @@ export class HttpConfigService implements HttpModuleOptionsFactory {
     createHttpOptions(): HttpModuleOptions {
         return {
             headers: {
-                'Authorization': 'Bearer EAAGG3jm0nTkBACNqCQBCESYVlwnwTpXOL3S5TyKZCCIW40FFKz1ubXHu5p6j4wOIheZCRI2FRZCfDgIGBZAmdZBPjbyKGKm6eIkqHlaGwNHbZBbiTCoKC6rNVoHAHmSOpOhfnzHWec2d9ZCeFZCZBUKBhWxan6EP2UDrugRzsTtyzzkkk8UI09JR5lZCfS74PAODWWy5w3ZAmzd5QZDZD',
+                'Authorization': 'Bearer EAAGG3jm0nTkBAEl0VhZAr3aNXVApLySjoyuepZBpLZC94LFfx6ZAIrzTxUqZCn4Ihs2EzhtAHHLfj2GDoRVGZC9ZC8imMbIZCotmZA83RZBAdo2BcZAP7ny4GXkKWWUe7fZCg0gd7ZBPlZCbmXPcaExXFdsN9Dlog1ZBTYkI8OkTtmEip7N3M0DFQ9hbXJuk6E16V3iL3F8dGOMkSaM0QZDZD',
                 'Content-Type': 'application/json'
             }
         }
